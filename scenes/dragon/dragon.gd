43d38e5b923197abd322f4f710104601ff877c16
Nelson López <nel_javier@outlook.com>
2024-05-27 15:00:28 -0400

    commit

--- scenes/dragon/dragon.gd
+++ scenes/dragon/dragon.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const Gravedad: float = 80.0
 const Power: float = -300.0
 
+@onready var sprite = $Sprite
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,8 +22,16 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
+	if is_on_floor() == true:
+		morir()
+	
 	
 	
 func volar() -> void:
 	if Input.is_action_just_pressed("volar") == true:
 		velocity.y = Power
+
+
+func morir() -> void:
+	sprite.stop()
+	set_physics_process(false)
